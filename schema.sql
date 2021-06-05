00db73be23d2b90af3b19857cddd40f9fa0c293d
Maria Klimnikova <MKlimnikova@renins.com>
2021-06-05 20:42:45 +1000

    add schema for database

---
+++ schema.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE buy_sell
+  WITH OWNER =  academy
+  ENCODING = 'UTF8'
+  TEMPLATE = template0
+  LC_COLLATE = 'C'
+  LC_CTYPE = 'C'
+  CONNECTION LIMIT = -1;
+
+DROP TABLE IF EXISTS offer_categories;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS offers;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS users;
+
+
+CREATE TABLE categories(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name varchar(255) NOT NULL
+);
+
+CREATE TABLE users(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  email varchar(255) NOT NULL,
+  password_hash varchar(255) NOT NULL,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  avatar varchar(50) NOT NULL
+);
+
+CREATE TABLE offers(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title varchar(255) NOT NULL,
+  description text NOT NULL,
+  sum integer NOT NULL,
+  type varchar(5) NOT NULL,
+  picture varchar(50) NOT NULL,
+  user_id INTEGER NOT NULL,
+  created_at timestamp DEFAULT current_timestamp,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE comments(
+  id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  offer_id integer NOT NULL,
+  user_id integer NOT NULL,
+  text text NOT NULL,
+  created_at timestamp DEFAULT current_timestamp,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (offer_id) REFERENCES offers(id)
+);
+
+CREATE TABLE offer_categories(
+  offer_id integer NOT NULL,
+  category_id integer NOT NULL,
+  PRIMARY KEY (offer_id, category_id),
+  FOREIGN KEY (offer_id) REFERENCES offers(id),
+  FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+CREATE INDEX ON offers(title);
